a9d62253150e9a2cdbb6c6ca503d3c6d275280b7
nevsiruj <98376961+nevsiruj@users.noreply.github.com>
2023-05-28 21:53:03 +0000

    Update ListSocio.vue

--- src/components/socios/ListSocio.vue
+++ src/components/socios/ListSocio.vue
@@ -198,7 +198,7 @@
                             class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                             @click="cargarRecibo(socio.dni)"
                           >
-                            Cargar recibo al socio
+                            Cargar recibo
                           </a>
                         </li>
 
